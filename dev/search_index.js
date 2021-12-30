8c7f23c1b34b85dfb3e10528eea3bfca8c40b1e3
Documenter.jl <documenter@juliadocs.github.io>
2021-12-30 15:47:37 +0000

    build based on c8a1476

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = CxxCall","category":"page"},{"location":"#CxxCall","page":"Home","title":"CxxCall","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CxxCall.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [CxxCall]","category":"page"}]
+}
