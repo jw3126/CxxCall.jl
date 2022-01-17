c67d874159a793974dd7db679f9092770228c5fb
Jan Weidner <jw3126@gmail.com>
2022-01-17 16:59:48 +0100

    remove a bit of boilerplate from test_hello_world.jl

--- test/test_hello_world.jl
+++ test/test_hello_world.jl
@@ -1,36 +1,33 @@
 module TestAdd
 
-module Wrapper
-    using CxxCall
-    dir = mktempdir()
-    lib = joinpath(dir, "libHelloWorld")
-    filepath = joinpath(dir, "HelloWorld.cxx")
-    eval(cxxsetup())
-    eval(cxxnewfile(filepath,
+using CxxCall
+dir = mktempdir()
+lib = joinpath(dir, "libHelloWorld")
+filepath = joinpath(dir, "HelloWorld.cxx")
+eval(cxxsetup())
+eval(cxxnewfile(filepath,
+"""
+#include <iostream>
+"""))
+
+@cxx lib function hello(name::Cstring)::Cvoid
     """
-    #include <iostream>
-    """))
-    
-    @cxx lib function hello(name::Cstring)::Cvoid
-        """
-        std::cout << "Hello " << name << "!" << std::endl;
-        """
-    end
-end#module Wrapper
+    std::cout << "Hello " << name << "!" << std::endl;
+    """
+end
 
 using Test
-import .Wrapper
 using Libdl
 
 @testset "hello_world" begin
-    @test !ispath(Wrapper.filepath)
-    Wrapper.cxx_write_code!()
-    @test isfile(Wrapper.filepath)
-    libpath = "$(Wrapper.lib).$dlext"
-    run(`g++ -shared -fPIC $(Wrapper.filepath) -o $libpath`)
+    @test !ispath(filepath)
+    cxx_write_code!()
+    @test isfile(filepath)
+    libpath = "$(lib).$dlext"
+    run(`g++ -shared -fPIC $(filepath) -o $libpath`)
     @test isfile(libpath)
     dlopen(libpath)
-    Wrapper.hello("world")
+    hello("world")
 end
 
 end#module
